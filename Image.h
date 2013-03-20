cab1c797585a3bb4fd6a336e0a5e04a810f146c0
Simon Jarbrant <simonjarbrant@gmail.com>
2013-03-20 20:40:12 +0100

    Changed IMG_Load to use char* instead of string

--- Image.h
+++ Image.h
@@ -1,17 +1,17 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
-#include <SDL_image.h>
+#include <SDL/SDL_image.h>
 #include <string>
 
 class Image {
 	public:
 		Image();
-		Image(std::string filename, bool transparent_background);
+		Image(char* filename, bool transparent_background);
 		~Image();
 	private:
 		SDL_Surface *image;
-		std::string filename;
+		char* filename;
 		bool transparent_background; //En boolean som anger om bildens bakgrund ska vara genomskinlig eller ej
 };
 
